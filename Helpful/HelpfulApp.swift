c2f6e87ce704de8556b99759a01497729cd77640
Donta Delafose <DontaDelafose11092001@gmail.com>
2026-02-09 12:25:12 -0600

    Initial Commit

---
+++ Helpful/HelpfulApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelpfulApp.swift
+//  Helpful
+//
+//  Created by CAIT on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct HelpfulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
